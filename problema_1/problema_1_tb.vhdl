317e406d57599472e6555d1fbbb694a07d8e6828
Alejandro Naser Pastoriza <alejnaser@gmail.com>
2013-08-16 22:20:53 -0300

    fixed test

--- problema_1/problema_1_tb.vhdl
+++ problema_1/problema_1_tb.vhdl
@@ -6,6 +6,15 @@ entity problema_1_tb is
 end entity;
 
 architecture test_bench of problema_1_tb is
+  function to_string (sv: Std_Logic_Vector) return string is
+    use Std.TextIO.all;
+    variable bv: bit_vector(sv'range) := to_bitvector(sv);
+    variable lp: line;
+  begin
+    write (lp, bv);
+    return lp.all;
+  end;
+
   component fulladder32bits
     port (a, b : in std_logic_vector((width - 1) downto 0);
           y : out std_logic_vector(width downto 0));
@@ -18,19 +27,23 @@ begin
   begin
     sa <= (0 => '1', others => '0');
     sb <= (0 => '1', others => '0');
---    assert sy = "000000000000000000000000000000010";
     wait for 5 ns;
+--    report "sy vector value: " & to_string(sy);
+    assert sy = "000000000000000000000000000000010";
     sa <= (5 => '1', others => '0');
     sb <= (5 => '1', others => '0');
---    assert sy = x"000000000000000000000000001000000";
     wait for 5 ns;
+--    report "sy vector value: " & to_string(sy);
+    assert sy = "000000000000000000000000001000000";
     sa <= (10 => '1', others => '0');
     sb <= (10 => '1', others => '0');
---    assert sy = x"000000000000000000000100000000000";
     wait for 5 ns;
+--    report "sy vector value: " & to_string(sy);
+    assert sy = "000000000000000000000100000000000";
     sa <= "11101001010001010001011110011100";
     sb <= "11110010010011000110001110100110";
---    assert sy = x"111011011100100010111101101000010";
     wait for 5 ns;
+--    report "sy vector value: " & to_string(sy);
+    assert sy = "111011011100100010111101101000010";
   end process;
 end architecture;

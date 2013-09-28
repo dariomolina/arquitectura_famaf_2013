6caee71ba238e4f474c5622d542f653f4ef7168d
Alejandro Naser Pastoriza <alejnaser@gmail.com>
2013-09-28 17:40:09 -0300

    problema 2

--- practico_2/problema_2/problema_2_tb.vhdl
+++ practico_2/problema_2/problema_2_tb.vhdl
@@ -5,112 +5,103 @@ entity problema_2_tb is
 end entity;
 
 architecture ar_maindec_tb of problema_2_tb is
-    component maindec
-    port (
-          Op         : in  std_logic_vector(5 downto 0);
-          MemToReg   : out std_logic;
-          MemWrite   : out std_logic;
-          Branch     : out std_logic;
-          AluSrc     : out std_logic;
-          RegDst     : out std_logic;
-          RegWrite   : out std_logic;
-          Jump       : out std_logic;
-          Aluop      : out std_logic_vector(1 downto 0));
-      end component;
+  component maindec
+    port (Op       : in  std_logic_vector(5 downto 0);
+          MemToReg : out std_logic;
+          MemWrite : out std_logic;
+          Branch   : out std_logic;
+          AluSrc   : out std_logic;
+          RegDst   : out std_logic;
+          RegWrite : out std_logic;
+          Jump     : out std_logic;
+          Aluop    : out std_logic_vector(1 downto 0));
+  end component;
 
-    signal Op : std_logic_vector(5 downto 0);
-    signal MemToReg, MemWrite, Branch, AluSrc, RegDst, RegWrite, Jump : std_logic;
-    signal AluOp : std_logic_vector(1 downto 0);
+  signal sOp : std_logic_vector(5 downto 0);
+  signal sMemToReg, sMemWrite, sBranch, sAluSrc, sRegDst, sRegWrite, sJump : std_logic;
+  signal sAluOp : std_logic_vector(1 downto 0);
 
-    begin
-        mapeo : maindec port map (Op => Op, MemToReg => MemToReg, MemWrite => MemWrite,
-                                    Branch => Branch, AluSrc => AluSrc, RegDst => RegDst,
-                                    RegWrite => RegWrite, Jump => Jump, Aluop => Aluop);
-        process
-            begin
-                wait for 5 ns;
-                Op <= "000000";
-                wait for 5 ns;
-                assert RegWrite = '1';
-                assert RegDst   = '1';
-                assert AluSrc   = '0';
-                assert Branch   = '0';
-                assert MemWrite = '0';
-                assert MemToReg = '0';
-                assert Jump     = '0';
-                assert AluOp    = "10";
+begin
+  tb_componente : maindec port map (Op => sOp, MemToReg => sMemToReg, MemWrite => sMemWrite,
+                                    Branch => sBranch, AluSrc => sAluSrc, RegDst => sRegDst,
+                                    RegWrite => sRegWrite, Jump => sJump, Aluop => sAluop);
+  process
+  begin
+    sOp <= "000000";
+    wait for 5 ns;
+    assert sRegWrite = '1';
+    assert sRegDst   = '1';
+    assert sAluSrc   = '0';
+    assert sBranch   = '0';
+    assert sMemWrite = '0';
+    assert sMemToReg = '0';
+    assert sJump     = '0';
+    assert sAluOp    = "10";
 
-                wait for 5 ns;
-                Op <= "100011";
-                wait for 5 ns;
-                assert RegWrite = '1';
-                assert RegDst   = '0';
-                assert AluSrc   = '1';
-                assert Branch   = '0';
-                assert MemWrite = '0';
-                assert MemToReg = '1';
-                assert Jump     = '0';
-                assert AluOp    = "00";
+    sOp <= "100011";
+    wait for 5 ns;
+    assert sRegWrite = '1';
+    assert sRegDst   = '0';
+    assert sAluSrc   = '1';
+    assert sBranch   = '0';
+    assert sMemWrite = '0';
+    assert sMemToReg = '1';
+    assert sJump     = '0';
+    assert sAluOp    = "00";
 
-                wait for 5 ns;
-                Op <= "101011";
-                wait for 5 ns;
-                assert RegWrite = '0';
-                assert RegDst   = '0';
-                assert AluSrc   = '1';
-                assert Branch   = '0';
-                assert MemWrite = '1';
-                assert MemToReg = '0';
-                assert Jump     = '0';
-                assert AluOp    = "00";
+    sOp <= "101011";
+    wait for 5 ns;
+    assert sRegWrite = '0';
+    assert sRegDst   = '0';
+    assert sAluSrc   = '1';
+    assert sBranch   = '0';
+    assert sMemWrite = '1';
+    assert sMemToReg = '0';
+    assert sJump     = '0';
+    assert sAluOp    = "00";
 
-                wait for 5 ns;
-                Op <= "000100";
-                wait for 5 ns;
-                assert RegWrite = '0';
-                assert RegDst   = '0';
-                assert AluSrc   = '0';
-                assert Branch   = '1';
-                assert MemWrite = '0';
-                assert MemToReg = '0';
-                assert Jump     = '0';
-                assert AluOp    = "10";
+    sOp <= "000100";
+    wait for 5 ns;
+    assert sRegWrite = '0';
+    assert sRegDst   = '0';
+    assert sAluSrc   = '0';
+    assert sBranch   = '1';
+    assert sMemWrite = '0';
+    assert sMemToReg = '0';
+    assert sJump     = '0';
+    assert sAluOp    = "01";
 
-                wait for 5 ns;
-                Op <= "001000";
-                wait for 5 ns;
-                assert RegWrite = '1';
-                assert RegDst   = '0';
-                assert AluSrc   = '1';
-                assert Branch   = '0';
-                assert MemWrite = '0';
-                assert MemToReg = '0';
-                assert Jump     = '0';
-                assert AluOp    = "00";
+    sOp <= "001000";
+    wait for 5 ns;
+    assert sRegWrite = '1';
+    assert sRegDst   = '0';
+    assert sAluSrc   = '1';
+    assert sBranch   = '0';
+    assert sMemWrite = '0';
+    assert sMemToReg = '0';
+    assert sJump     = '0';
+    assert sAluOp    = "00";
 
-                wait for 5 ns;
-                Op <= "000010";
-                wait for 5 ns;
-                assert RegWrite = '0';
-                assert RegDst   = '0';
-                assert AluSrc   = '0';
-                assert Branch   = '0';
-                assert MemWrite = '0';
-                assert MemToReg = '0';
-                assert Jump     = '1';
-                assert AluOp    = "00";
+    sOp <= "000010";
+    wait for 5 ns;
+    assert sRegWrite = '0';
+    assert sRegDst   = '0';
+    assert sAluSrc   = '0';
+    assert sBranch   = '0';
+    assert sMemWrite = '0';
+    assert sMemToReg = '0';
+    assert sJump     = '1';
+    assert sAluOp    = "00";
 
-                wait for 5 ns;
-                Op <= "111111";
-                wait for 5 ns;
-                assert RegWrite = '0';
-                assert RegDst   = '0';
-                assert AluSrc   = '0';
-                assert Branch   = '0';
-                assert MemWrite = '0';
-                assert MemToReg = '0';
-                assert Jump     = '0';
-                assert AluOp    = "00";
-
-        end process;
+    sOp <= "111111";
+    wait for 5 ns;
+    assert sRegWrite = '-';
+    assert sRegDst   = '-';
+    assert sAluSrc   = '-';
+    assert sBranch   = '-';
+    assert sMemWrite = '-';
+    assert sMemToReg = '-';
+    assert sJump     = '-';
+    assert sAluOp    = "--";
+  end process;
 end architecture;

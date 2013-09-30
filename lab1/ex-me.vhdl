ed8a19bd084cc93a77ffebff9f4157e93860f570
Mariano Mateuci <mjmateuci@gmail.com>
2013-09-30 19:12:25 -0300

    perdonenme, me olvide de hacer add jaja

---
+++ lab1/ex-me.vhdl
@@ -0,0 +1,10 @@
+library ieee;
+library work;
+use work.components.all;
+use ieee.std_logic_1164.all;
+
+entity ex_me is
+end entity;
+
+architecture behavior of ex_me is
+end architecture;
---
+++ lab1/me-wb.vhdl
@@ -0,0 +1,10 @@
+library ieee;
+library work;
+use work.components.all;
+use ieee.std_logic_1164.all;
+
+entity me_wb is
+end entity;
+
+architecture behavior of me_wb is
+end architecture;
